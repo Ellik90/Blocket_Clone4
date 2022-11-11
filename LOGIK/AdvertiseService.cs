e33e3eb73123c3fcd066b4d7313d73790bb8ed2f
Daniel Svan <daniel_svan1@hotmail.com>
2022-11-11 13:04:12 +0100

    arbete i advertiseservice

--- LOGIK/AdvertiseService.cs
+++ LOGIK/AdvertiseService.cs
@@ -1,5 +1,18 @@
-class AdvertiseService
+namespace LOGIK;
+public class AdvertiseService
 {
+    //Sökmetod 
+    IAdManagement _IadManager; 
+    List <advertise> listOfAdvertise = new List<advertise>();
+
+  public AdvertiseService(IAdManagement _IadManager)
+  {
+    this._IadManager = _IadManager;
+  }
+  public void MakeNewAd(advertise advertise)
+  {
+    _IadManager.CreateAd(advertise);
+  }
     // här finns funktioner som hanterar advertise, mellan användare och databasen
     //här in behövs ju då komma ett interface IAdManager adManager; behöver finnas tex via konstruktorn
     // tex  makenewad(string rubric, float price....);
