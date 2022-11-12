3e7291005f04cc70e4e8432b087361dae798d7dd
Angelina Holmqvist <angelinaholmqvist@live.se>
2022-11-12 12:38:29 +0100

    commit

--- UI/Program.cs
+++ UI/Program.cs
@@ -27,7 +27,7 @@ internal class Program
 
         //2. LOGGA IN PÅ BEFINTLIGT KONTO
         user = new();  //elinak90@icloud.com meleklina@outlook.com
-        user.Email = "meleklina@outlook.com";//ConsoleInput.GetString("Enter your Email");
+        user.Email = "angelinaholmqvist@live.se";//ConsoleInput.GetString("Enter your Email");
         user.Password = 1010;//ConsoleInput.GetInt("Enter your Password");
         user = logInService.UserLogIn(user); //user skriver bara i sin mail och kod
         user.Id = logInService.UserLogInIsValid(user); //andvänder userhandler och ser om user finns
