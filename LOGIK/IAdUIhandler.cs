d3496795e8edc2e30787dd24f1596c1dbf26824c
Angelina Holmqvist <angelinaholmqvist@live.se>
2022-11-14 15:25:18 +0100

    new commit

--- LOGIK/IAdUIhandler.cs
+++ LOGIK/IAdUIhandler.cs
@@ -3,11 +3,8 @@ namespace LOGIK
     public interface IAdUIhandler
     {
         public int MakeNewAd(Advertise advertise);
-
         public List<Advertise> SearchAd(string search);
-
         public void RemoveOneAd(int id);
-
         public Advertise ShowOneAd(int id);
     }
 }
