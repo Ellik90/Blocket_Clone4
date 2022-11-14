039213caeb4945f5ac21b6f75062c4704af27152
Daniel Svan <daniel_svan1@hotmail.com>
2022-11-14 14:14:20 +0100

    show ad

--- LOGIK/AdvertiseService.cs
+++ LOGIK/AdvertiseService.cs
@@ -47,6 +47,11 @@ public class AdvertiseService
         }
 
     }
+    public advertise ShowOneAd(int id)
+    {
+        return _IadManager.ShowAd(id);
+       
+    }
     // här finns funktioner som hanterar advertise, mellan användare och databasen
     //här in behövs ju då komma ett interface IAdManager adManager; behöver finnas tex via konstruktorn
     // tex  makenewad(string rubric, float price....);
