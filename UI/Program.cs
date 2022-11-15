65fe2a2d04bca32a088a81cd87d36ac6f97eb039
Angelina Holmqvist <angelinaholmqvist@live.se>
2022-11-15 12:26:03 +0100

    leginservice interface

--- UI/Program.cs
+++ UI/Program.cs
@@ -81,7 +81,7 @@ internal class Program
                     admin.Email = ConsoleInput.GetString("Enter your Email");
                     admin.PassWord = ConsoleInput.GetInt("Enter your Password");
                     admin = logInService.AdminLogIn(admin); //user skriver bara i sin mail och kod
-                    admin.Id = logInService.AdminLogInIsValic(admin); //andvänder userhandler och ser om user finns
+                    admin.Id = logInService.AdminLogInIsValid(admin); //andvänder userhandler och ser om user finns
                     if (admin.Id == 0) //<- tex om user är inloggad då så kommer man till user page?
                     {
                         Console.WriteLine("Fel lösen eller mail");
