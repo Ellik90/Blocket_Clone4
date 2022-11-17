c4893daa38dadfc755a5a7ec4347fb77561f376e
Angelina Holmqvist <angelinaholmqvist@live.se>
2022-11-17 12:33:58 +0100

    testar

--- DATABASE/IUserEditor.cs
+++ DATABASE/IUserEditor.cs
@@ -5,4 +5,5 @@ public interface IUserEditor
     public int UpdateEmail(User user, string userEmail);
     public int UpdateNickName(User user, string nickname);
     public int UpDateDescription(User user, string updateDescription);
+    //hej
 }
