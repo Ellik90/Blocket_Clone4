f8c077501178809fcb673e14160b0a1cf25b6764
Elina Kerola <elinak90@icloud.com>
2022-11-21 18:06:35 +0100

    db

--- DATABASE/AdminDB.cs
+++ DATABASE/AdminDB.cs
@@ -16,39 +16,7 @@ public class AdminDB : IAdminHandeler
         }
         return rows;
     }
-    // public int AdminLogInExists(Admin admin)
-    // {
-    //     // EGEN DB KLASS
-    //     int id = 0;
-    //     using (MySqlConnection connection = new MySqlConnection($"Server=localhost;Database=Blocket_clone;Uid=root;Pwd=;"))
-    //     {
-    //         string? query = "SELECT * FROM admins WHERE email = @email AND pass_word = @password; SELECT LAST_INSERT_ID() ";
-    //         id = connection.ExecuteScalar<int>(query, param: admin);
-    //         return id;
-    //     }
-    // }
-    // public bool AdminEmailExists(string Email)
-    // {
-    //     //EGEN DB KLASS?
-    //     bool rows = true;
-    //     using (MySqlConnection connection = new MySqlConnection($"Server=localhost;Database=Blocket_clone;Uid=root;Pwd=;"))
-    //     {
-    //         string? query = "SELECT * FROM admins WHERE email = @email";
-    //         rows = connection.ExecuteScalar<bool>(query, new { @email = Email });
-    //     }
-    //     return rows;
-    // }
-    // public bool AdminNameExists(string name)
-    // {
-    //     //EGEN DB KLASS
-    //     bool rows = true;
-    //     using (MySqlConnection connection = new MySqlConnection($"Server=localhost;Database=Blocket_clone;Uid=root;Pwd=;"))
-    //     {
-    //         string? query = "SELECT * FROM admins WHERE admin_name = @name";
-    //         rows = connection.ExecuteScalar<bool>(query, new { @name = name });   // denna används inte
-    //     }
-    //     return rows;
-    // }
+  
     public int DeleteAdmin(Admin admin)
     {
         int rows = 0;
